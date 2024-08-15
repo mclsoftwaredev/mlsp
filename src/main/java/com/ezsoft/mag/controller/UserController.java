3cb46762665031c0283729937a32f669d124acbf
Michael Lee <michael.lee@ticketmaster.com>
2024-08-15 04:25:13 -0400

    All functions working

--- src/main/java/com/ezsoft/mag/controller/UserController.java
+++ src/main/java/com/ezsoft/mag/controller/UserController.java
@@ -7,21 +7,27 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import com.ezsoft.mag.data.User;
 
+import java.sql.Date;
 import java.util.List;
 
-@RestController("/user")
+@RestController()
 public class UserController {
 
     @Autowired
     UserRepository repository;
 
-    @GetMapping("/{id}")
+    @GetMapping("/user/{id}")
     public User getUserById(@PathVariable("id") String id) {
         return repository.findById(Long.parseLong(id));
     }
 
-    @GetMapping("/profession/{profession}")
+    @GetMapping("/user/profession/{profession}")
     public List<User> getUserByProfession(@PathVariable("profession") String profession) {
         return repository.findByProfession(profession);
+    }
+
+    @GetMapping("/user/startDate/{start}/endDate/{end}")
+    public List<User> getUserByDateRange(@PathVariable("start") String start, @PathVariable("end") String end) {
+        return repository.findByDateCreatedBetween(Date.valueOf(start), Date.valueOf(end));
     }
 }
